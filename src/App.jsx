4991cb641963d68a3f7b62a569b8bc6a656ad8ff
Camila Juarez Ascaini <camilajuarez_96@hotmail.com>
2023-06-19 13:12:00 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './component/header'
+
+
+function App() {
+
+  return (
+    <div>
+      <Header logo="MonsterMarket" />
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/header/index.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+import './styles.css';
+
+const Header = ( {logo, menuItem} ) => {
+    return (
+        <header className="header">
+            <a href="/" className="logo">{logo}</a>
+            <input type="checkbox" className="side-menu" id="side-menu"></input>
+            <label className="hamb" for="side-menu">
+                <span className="hamb-line"></span>
+            </label>
+            <nav className="nav">
+                <ul className="menu">
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Product</a></li>
+                    <li><a href="#">Categories</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                {/* <ul>
+                    <img src=".../assets/img/carritoLogo.png" alt="carritoLogo" />
+                </ul> */}
+            </nav>
+        </header>
+    )
+}
+
+
+export default Header;
+
+
+
